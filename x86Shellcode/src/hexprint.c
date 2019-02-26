631d1180310efee3c84018663900bdfc71509853
oonray <alexanderbjornsrid@gmail.com>
2019-02-26 15:52:46 +0100

    added shellcode printer

---
+++ x86Shellcode/src/hexprint.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+extern void shellstart(void);
+extern void shellstop(void);
+
+int main(){
+    unsigned char *start = (unsigned char *) shellstart;
+    unsigned char *stop = (unsigned char *) shellstop;
+    while(start != stop){
+        printf("%02x ",*start);
+        start++;
+    }
+    return 0;
+}
