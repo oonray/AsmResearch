b48e6204044d03133d41e3a22d7bb449939fc974
oonray <alexanderbjornsrud@gmail.com>
2020-08-04 22:56:22 +0100

    .

--- x86Shellcode/src/hexprint.c
+++ x86Shellcode/src/hexprint.c
@@ -4,11 +4,21 @@ extern void shellstart(void);
 extern void shellstop(void);
 
 int main(){
+
+    unsigned char *abs_start = (unsigned char *) shellstart;
     unsigned char *start = (unsigned char *) shellstart;
     unsigned char *stop = (unsigned char *) shellstop;
     while(start != stop){
         printf("\\x%02x",*start);
         start++;
     }
+    start = abs_start;
+
+    printf("\n{");
+    while(start != stop){
+        printf("0x%02x,",*start);
+        start++;
+    }
+    printf("};");
     return 0;
 }
